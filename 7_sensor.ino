e023c7e33c4b43047e4d138567818bff6cfcb9c6
Lachllenn <57431472+Lachllenn@users.noreply.github.com>
2019-11-07 13:55:24 +1100

    Update 7_sensor.ino

--- 7_sensor.ino
+++ 7_sensor.ino
@@ -7,8 +7,10 @@ int timer = 0;
 const char* ssid = "FPT UGlobal";
 const char* password = "123456789";
 String ip;
-const int trigPin = D3;
-const int echoPin = D4;
+const int trigPin1 = D1;
+const int echoPin1 = D2;
+const int trigPin2 = D3;
+const int echoPin2 = D4;
 long duration, cm, inches;
 void setup() {
   // put your setup code here, to run once:
@@ -35,29 +37,43 @@ void setup() {
 }
 
 void loop() {
-  //time counter//
-  int hour, minute, second;
-  delay(1000);
-  timer++;
-  hour = timer / 3600;
-  minute = (timer % 3600) / 60;
-  second = timer % 60;
-  //ultrasonic//
-   digitalWrite(trigPin, LOW);
+  //ultrasonic 1//
+   digitalWrite(trigPin1, LOW);
    delayMicroseconds(5);
-   digitalWrite(trigPin, HIGH);
+   digitalWrite(trigPin1, HIGH);
    delayMicroseconds(10);
-   digitalWrite(trigPin, LOW);
-   pinMode(echoPin, INPUT);
-   duration = pulseIn(echoPin, HIGH);
+   digitalWrite(trigPin1, LOW);
+   pinMode(echoPin1, INPUT);
+   duration1 = pulseIn(echoPin1, HIGH);
+   //ultrasonic 2//
+   digitalWrite(trigPin2, LOW);
+   delayMicroseconds(5);
+   digitalWrite(trigPin2, HIGH);
+   delayMicroseconds(10);
+   digitalWrite(trigPin2, LOW);
+   pinMode(echoPin2, INPUT);
+   duration2 = pulseIn(echoPin2, HIGH);
+  
+  
+  
+  
 
    // Convert the time into a distance//
-  cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
-  inches = (duration/2) / 74;   // Divide by 74 or multiply by 0.0135
+  s1cm = (duration1/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
+  s1inches = (duration1/2) / 74;   // Divide by 74 or multiply by 0.0135
   
-  Serial.print(inches);
+  s2cm = (duration2/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
+  s2inches = (duration2/2) / 74;   // Divide by 74 or multiply by 0.0135
+  
+  //serial output//
+  Serial.print(s1inches);
   Serial.print("in,");
-  Serial.print(cm);
+  Serial.print(s1cm);
+  Serial.print("cm");
+  Serial.println();
+  Serial.print(s2inches);
+  Serial.print("in,");
+  Serial.print(s2cm);
   Serial.print("cm");
   Serial.println();
 }
